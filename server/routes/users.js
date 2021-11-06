7ee7ae34b79822ad076765ad2a83d6a2cc9d4930
Mert Ture <mertture@Mert-MacBook-Pro.local>
2021-11-07 01:13:22 +0300

    CF-12 feat: encrypt password before database insertion

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const bcrypt = require("bcryptjs")
 // userRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /users.
@@ -26,48 +26,72 @@ userRoutes.route("/users").get(function (req, res) {
 
 // This section will help you get a single users by id
 userRoutes.route("/users/:id").get(function (req, res) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
+  let db_connect = dbo.getDb("courseflow");
+  let myquery = { _id: ObjectId(req.params.id) };
   db_connect
-      .collection("users")
-      .findOne(myquery, function (err, result) {
-        if (err) throw err;
-        res.json(result);
-      });
+    .collection("users")
+    .findOne(myquery, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
 });
 
 // This section will help you create a new users.
 userRoutes.route("/users/add").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myobj = {
+
+  let db_connect = dbo.getDb("courseflow");
+
+  let user = {
     username: req.body.username,
     university: req.body.university,
     email: req.body.email,
-    person_position: req.body.password,
+    password: req.body.password,
   };
-  // db_connect.collection("users").createIndex( { email: 1 }, { unique: true } );
-  db_connect.collection("users").find({"email": req.body.email}).toArray()
-  .then((result) => {
-    if(result.length === 0){
-      console.log('success')
-      db_connect.collection("users").insertOne(myobj, function (err, res) {
-        if (err) throw err;
-        response.json(res);
-      });
-    }
-    else{
-      // console.log('success')
-      // throw 'This email has already been used';
-      response.json({message: 'This email has already been used!'});
+
+  // hashing time
+  const saltRounds = 10;
+
+  // encrypting the password
+  bcrypt.genSalt(saltRounds, function (saltError, salt) {
+    if (saltError) {
+      return saltError
+    } else {
+      bcrypt.hash(req.body.password, salt, function (hashError, hash) {
+        if (hashError) {
+          return hashError
+        }
+
+        user.password = hash;
+
+        // inserting to users collection after hashing
+        db_connect.collection("users").find({ "email": req.body.email }).toArray()
+          .then((result) => {
+            if (result.length === 0) {
+              db_connect.collection("users").insertOne(user, function (err, res) {
+                if (err) throw err;
+                response.json(res);
+              });
+            }
+            else {
+              response.json({ message: 'This email has already been used!' });
+            }
+          })
+      })
     }
   })
-  
+
+  // user information
+
+
+
+
+
 });
 
 // This section will help you update a users by id.
 userRoutes.route("/update/:id").post(function (req, response) {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
+  let myquery = { _id: ObjectId(req.params.id) };
   let newvalues = {
     $set: {
       person_name: req.body.person_name,
@@ -87,7 +111,7 @@ userRoutes.route("/update/:id").post(function (req, response) {
 // This section will help you delete a users
 userRoutes.route("/:id").delete((req, response) => {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
+  let myquery = { _id: ObjectId(req.params.id) };
   db_connect.collection("users").deleteOne(myquery, function (err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
