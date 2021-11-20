28d8595e1ecdff234b30e4cc2e50eee29e8a8984
mizbahc <m.mzbh@outlook.com>
2021-11-21 00:25:34 +0300

    feat: add error page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ErrorPage from "./pages/error-page/error-page.jsx";
 import Homepage from "./pages/homepage/homepage.jsx";
 import Login from "./pages/login/login.jsx";
 import Signup from "./pages/signup/signup.jsx";
@@ -29,11 +30,12 @@ function App() {
         />
         <Route
           path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>Error! There&apos;s nothing here!</p>
-            </main>
-          }
+          element={<ErrorPage />}
+          // element={
+          //   <main style={{ padding: "1rem" }}>
+          //     <p>Error! There&apos;s nothing here!</p>
+          //   </main>
+          // }
         />
       </Routes>
     </React.Suspense>
