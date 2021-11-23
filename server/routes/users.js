093b6b15f9df6896f9ad117ebe2f89af365741c7
mizbahc <m.mzbh@outlook.com>
2021-11-23 09:51:34 +0300

    update deploy changed scripts

--- server/routes/users.js
+++ server/routes/users.js
@@ -184,7 +184,7 @@ userRoutes.post('/forgotPassword', (req, res) => {
         text:
           'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
           + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
-          + `http://localhost:3000/reset/${token}\n\n`
+          + `http://localhost:5000/reset/${token}\n\n`
           + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
       };
 
