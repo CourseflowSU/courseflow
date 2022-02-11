f971aa9a5117d5668e5231b86a9c375613418697
mizbahc <m.mzbh@outlook.com>
2022-02-11 19:07:42 +0300

    reinstall

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -10,12 +10,9 @@ module.exports = {
   },
   "extends": [
   ],
-  "parser": "@babel/eslint-parser",
+ 
   "parserOptions": {
-    "requireConfigFile": false,
-    "babelOptions": {
-      "presets": ["@babel/preset-react"]
-    },
+    
     "ecmaFeatures": {
       "jsx": true,
     },
